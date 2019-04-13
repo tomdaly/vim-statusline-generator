3a8c5bba644f54b5d0e1abf6e7184e63def374ba
tomdaly <tomdaly@noreply.users.github.com>
2019-04-13 11:12:28 +0100

    Move Generator initialisation to new file for webpage

--- Generator.js
+++ Generator.js
@@ -301,16 +301,3 @@ GeneratorDom.prototype.alignLeft = function(state) {
         document.getElementById("leftButton").setAttribute("style", "");
     }
 };
-
-
-const genDom = new GeneratorDom();
-document.getElementById("options").appendChild(genDom.init());
-document.getElementById("clearButton").addEventListener("click", function() {
-    genDom.clear();
-});
-document.getElementById("leftButton").addEventListener("click", function() {
-    genDom.alignLeft(true);
-});
-document.getElementById("rightButton").addEventListener("click", function() {
-    genDom.alignLeft(false);
-});
