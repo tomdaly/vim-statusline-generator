fd71b806469a9f0965c06fa4ebdd3e752ab4093b
tomdaly <tomdaly@noreply.users.github.com>
2019-04-16 18:54:51 +0100

    Refactor with standard code style

--- DomInit.js
+++ DomInit.js
@@ -1,53 +1,53 @@
-const genDom = new GeneratorDom();
-document.getElementById("options").appendChild(genDom.init());
-document.getElementById("clearButton").addEventListener("click", function() {
-    genDom.clear();
-});
-document.getElementById("leftButton").addEventListener("click", function() {
-    genDom.alignLeft(true);
-});
-document.getElementById("rightButton").addEventListener("click", function() {
-    genDom.alignLeft(false);
-});
-document.getElementById("defaultButton").addEventListener("click", function() {
-    genDom.clear();
-    genDom.alignLeft(true);
-    genDom.generator.addElement(findElement("<font color=\"red\">Red foreground</font>"), genDom.leftAlign);
-    genDom.generator.addElement(findElement("Mode"), genDom.leftAlign);
-    genDom.generator.addElement(findElement("<font color=\"black\">Reset foreground</font>"), genDom.leftAlign);
-    genDom.generator.addElement(findElement("Space character"), genDom.leftAlign);
-    genDom.generator.addElement(findElement("Left chevron character"), genDom.leftAlign);
-    genDom.generator.addElement(findElement("Left chevron character"), genDom.leftAlign);
-    genDom.generator.addElement(findElement("Space character"), genDom.leftAlign);
-    genDom.generator.addElement(findElement("Short filename"), genDom.leftAlign);
-    genDom.generator.addElement(findElement("Space character"), genDom.leftAlign);
-    genDom.generator.addElement(findElement("Right chevron character"), genDom.leftAlign);
-    genDom.generator.addElement(findElement("Right chevron character"), genDom.leftAlign);
-    genDom.update();
-    genDom.alignLeft(false);
-    genDom.generator.addElement(findElement("Modified flag"), genDom.leftAlign);
-    genDom.generator.addElement(findElement("Help flag"), genDom.leftAlign);
-    genDom.generator.addElement(findElement("Read-only flag"), genDom.leftAlign);
-    genDom.generator.addElement(findElement("Space character"), genDom.leftAlign);
-    genDom.generator.addElement(findElement("<font color=\"blue\">Blue foreground</font>"), genDom.leftAlign);
-    genDom.generator.addElement(findElement("Git branch"), genDom.leftAlign);
-    genDom.generator.addElement(findElement("<font color=\"black\">Reset foreground</font>"), genDom.leftAlign);
-    genDom.generator.addElement(findElement("Space character"), genDom.leftAlign);
-    genDom.generator.addElement(findElement("Long filename"), genDom.leftAlign);
-    genDom.generator.addElement(findElement("Colon character"), genDom.leftAlign);
-    genDom.generator.addElement(findElement("Colon character"), genDom.leftAlign);
-    genDom.generator.addElement(findElement("<font color=\"green\">Green foreground</font>"), genDom.leftAlign);
-    genDom.generator.addElement(findElement("Current line number"), genDom.leftAlign);
-    genDom.generator.addElement(findElement("Slash character"), genDom.leftAlign);
-    genDom.generator.addElement(findElement("Total line number"), genDom.leftAlign);
-    genDom.generator.addElement(findElement("<font color=\"black\">Reset foreground</font>"), genDom.leftAlign);
-    genDom.generator.addElement(findElement("Pipe character"), genDom.leftAlign);
-    genDom.generator.addElement(findElement("File type"), genDom.leftAlign);
-    genDom.update();
-});
+const genDom = new GeneratorDom()
+document.getElementById('options').appendChild(genDom.init())
+document.getElementById('clearButton').addEventListener('click', function () {
+  genDom.clear()
+})
+document.getElementById('leftButton').addEventListener('click', function () {
+  genDom.alignLeft(true)
+})
+document.getElementById('rightButton').addEventListener('click', function () {
+  genDom.alignLeft(false)
+})
+document.getElementById('defaultButton').addEventListener('click', function () {
+  genDom.clear()
+  genDom.alignLeft(true)
+  genDom.generator.addElement(findElement('<font color="red">Red foreground</font>'), genDom.leftAlign)
+  genDom.generator.addElement(findElement('Mode'), genDom.leftAlign)
+  genDom.generator.addElement(findElement('<font color="black">Reset foreground</font>'), genDom.leftAlign)
+  genDom.generator.addElement(findElement('Space character'), genDom.leftAlign)
+  genDom.generator.addElement(findElement('Left chevron character'), genDom.leftAlign)
+  genDom.generator.addElement(findElement('Left chevron character'), genDom.leftAlign)
+  genDom.generator.addElement(findElement('Space character'), genDom.leftAlign)
+  genDom.generator.addElement(findElement('Short filename'), genDom.leftAlign)
+  genDom.generator.addElement(findElement('Space character'), genDom.leftAlign)
+  genDom.generator.addElement(findElement('Right chevron character'), genDom.leftAlign)
+  genDom.generator.addElement(findElement('Right chevron character'), genDom.leftAlign)
+  genDom.update()
+  genDom.alignLeft(false)
+  genDom.generator.addElement(findElement('Modified flag'), genDom.leftAlign)
+  genDom.generator.addElement(findElement('Help flag'), genDom.leftAlign)
+  genDom.generator.addElement(findElement('Read-only flag'), genDom.leftAlign)
+  genDom.generator.addElement(findElement('Space character'), genDom.leftAlign)
+  genDom.generator.addElement(findElement('<font color="blue">Blue foreground</font>'), genDom.leftAlign)
+  genDom.generator.addElement(findElement('Git branch'), genDom.leftAlign)
+  genDom.generator.addElement(findElement('<font color="black">Reset foreground</font>'), genDom.leftAlign)
+  genDom.generator.addElement(findElement('Space character'), genDom.leftAlign)
+  genDom.generator.addElement(findElement('Long filename'), genDom.leftAlign)
+  genDom.generator.addElement(findElement('Colon character'), genDom.leftAlign)
+  genDom.generator.addElement(findElement('Colon character'), genDom.leftAlign)
+  genDom.generator.addElement(findElement('<font color="green">Green foreground</font>'), genDom.leftAlign)
+  genDom.generator.addElement(findElement('Current line number'), genDom.leftAlign)
+  genDom.generator.addElement(findElement('Slash character'), genDom.leftAlign)
+  genDom.generator.addElement(findElement('Total line number'), genDom.leftAlign)
+  genDom.generator.addElement(findElement('<font color="black">Reset foreground</font>'), genDom.leftAlign)
+  genDom.generator.addElement(findElement('Pipe character'), genDom.leftAlign)
+  genDom.generator.addElement(findElement('File type'), genDom.leftAlign)
+  genDom.update()
+})
 
-function findElement(title) {
-    return options.find(elem => {
-        return elem.title === title;
-    });
+function findElement (title) {
+  return options.find(elem => {
+    return elem.title === title
+  })
 };
--- GeneratorSpec.js
+++ GeneratorSpec.js
@@ -1,152 +1,149 @@
-"use strict";
+'use strict'
 
 let testElement = {
-    out: "%m",
-    preview: "[+]",
-};
+  out: '%m',
+  preview: '[+]'
+}
 
-describe("testGenerator", function() {
+describe('testGenerator', function () {
+  let testGenerator
 
-    let testGenerator;
+  describe('Testing testGenerator', function () {
+    beforeEach(function () {
+      testGenerator = new Generator()
+    })
 
-    describe("Testing testGenerator", function() {
-        beforeEach(function() {
-            testGenerator = new Generator();
-        });
+    it('elements should be empty on initialise', function () {
+      expect(testGenerator.leftElements.length).toEqual(0)
+      expect(testGenerator.rightElements.length).toEqual(0)
+    })
 
-        it("elements should be empty on initialise", function() {
-            expect(testGenerator.leftElements.length).toEqual(0);
-            expect(testGenerator.rightElements.length).toEqual(0);
-        });
+    it('should clear elements', function () {
+      testGenerator.leftElements.push('a')
+      testGenerator.rightElements.push('a')
+      expect(testGenerator.leftElements.length).toEqual(1)
+      expect(testGenerator.rightElements.length).toEqual(1)
+      testGenerator.removeAllElements()
+      expect(testGenerator.leftElements.length).toEqual(0)
+      expect(testGenerator.rightElements.length).toEqual(0)
+    })
 
+    describe('if align is left', function () {
+      it('should add element to left', function () {
+        testGenerator.addElement(testElement, true)
+        expect(testGenerator.leftElements.length).toEqual(1)
+      })
 
-        it("should clear elements", function() {
-            testGenerator.leftElements.push("a");
-            testGenerator.rightElements.push("a");
-            expect(testGenerator.leftElements.length).toEqual(1);
-            expect(testGenerator.rightElements.length).toEqual(1);
-            testGenerator.removeAllElements();
-            expect(testGenerator.leftElements.length).toEqual(0);
-            expect(testGenerator.rightElements.length).toEqual(0);
-        });
+      it('should build statusline preview', function () {
+        testGenerator.leftElements.push(testElement)
+        let preview = testGenerator.buildPreview('left')
 
-        describe("if align is left", function() {
-            it("should add element to left", function() {
-                testGenerator.addElement(testElement, true);
-                expect(testGenerator.leftElements.length).toEqual(1);
-            });
+        expect(preview).toEqual('[+]')
+      })
 
-            it("should build statusline preview", function() {
-                testGenerator.leftElements.push(testElement);
-                let preview = testGenerator.buildPreview("left");
+      it('should build statusline code', function () {
+        testGenerator.leftElements.push(testElement)
+        let out = testGenerator.buildOutput()
 
-                expect(preview).toEqual("[+]");
-            });
+        expect(out).toEqual('set laststatus=2\nset statusline+=%m\n')
+      })
+    })
 
-            it("should build statusline code", function() {
-                testGenerator.leftElements.push(testElement);
-                let out = testGenerator.buildOutput();
+    describe('if align is right', function () {
+      it('should add element to right', function () {
+        testGenerator.addElement(testElement, false)
+        expect(testGenerator.rightElements.length).toEqual(1)
+      })
 
-                expect(out).toEqual("set laststatus=2\nset statusline+=%m\n");
-            });
-        });
+      it('should build statusline preview', function () {
+        testGenerator.rightElements.push(testElement)
+        let preview = testGenerator.buildPreview('right')
 
-        describe("if align is right", function() {
-            it("should add element to right", function() {
-                testGenerator.addElement(testElement, false);
-                expect(testGenerator.rightElements.length).toEqual(1);
-            });
+        expect(preview).toEqual('[+]')
+      })
 
-            it("should build statusline preview", function() {
-                testGenerator.rightElements.push(testElement);
-                let preview = testGenerator.buildPreview("right");
+      it('should build statusline code', function () {
+        testGenerator.rightElements.push(testElement)
+        let out = testGenerator.buildOutput()
 
-                expect(preview).toEqual("[+]");
-            });
+        expect(out).toEqual('set laststatus=2\nset statusline+=%=\nset statusline+=%m\n')
+      })
+    })
+  })
 
-            it("should build statusline code", function() {
-                testGenerator.rightElements.push(testElement);
-                let out = testGenerator.buildOutput();
+  describe('Testing webpage', function () {
+    let testGenDom, output, leftPreview, rightPreview, leftButton, rightButton
 
-                expect(out).toEqual("set laststatus=2\nset statusline+=%=\nset statusline+=%m\n");
-            });
-        });
-    });
+    beforeEach(function () {
+      output = document.createElement('input')
+      leftPreview = document.createElement('input')
+      rightPreview = document.createElement('input')
+      leftButton = document.createElement('button')
+      rightButton = document.createElement('button')
+      output.setAttribute('id', 'output')
+      leftPreview.setAttribute('id', 'leftPreview')
+      rightPreview.setAttribute('id', 'rightPreview')
+      leftButton.setAttribute('id', 'leftButton')
+      rightButton.setAttribute('id', 'rightButton')
+      document.body.appendChild(output)
+      document.body.appendChild(leftPreview)
+      document.body.appendChild(rightPreview)
+      document.body.appendChild(leftButton)
+      document.body.appendChild(rightButton)
+      testGenDom = new GeneratorDom()
+    })
 
-    describe("Testing webpage", function() {
+    afterEach(function () {
+      document.body.removeChild(output)
+      document.body.removeChild(leftPreview)
+      document.body.removeChild(rightPreview)
+      document.body.removeChild(leftButton)
+      document.body.removeChild(rightButton)
+    })
 
-        let testGenDom, output, leftPreview, rightPreview, leftButton, rightButton;
+    it('should initialise with buttons', function () {
+      const form = document.createElement('form')
+      let button
+      for (let i = 0; i < options.length; i++) {
+        button = document.createElement('button')
+        button.innerHTML = options[i].title
+        form.appendChild(button)
+      }
+      expect(testGenDom.init().childNodes.length).not.toBeLessThan(0)
+    })
 
-        beforeEach(function() {
-            output = document.createElement("input");
-            leftPreview = document.createElement("input");
-            rightPreview = document.createElement("input");
-            leftButton = document.createElement("button");
-            rightButton = document.createElement("button");
-            output.setAttribute("id", "output");
-            leftPreview.setAttribute("id", "leftPreview");
-            rightPreview.setAttribute("id", "rightPreview");
-            leftButton.setAttribute("id", "leftButton");
-            rightButton.setAttribute("id", "rightButton");
-            document.body.appendChild(output);
-            document.body.appendChild(leftPreview);
-            document.body.appendChild(rightPreview);
-            document.body.appendChild(leftButton);
-            document.body.appendChild(rightButton);
-            testGenDom = new GeneratorDom();
-        });
+    it('should update output', function () {
+      testGenDom.generator.addElement(testElement, true)
+      testGenDom.updateOutput()
 
-        afterEach(function() {
-            document.body.removeChild(output);
-            document.body.removeChild(leftPreview);
-            document.body.removeChild(rightPreview);
-            document.body.removeChild(leftButton);
-            document.body.removeChild(rightButton);
-        });
+      expect(output.value).toEqual('set laststatus=2set statusline+=%m')
+    })
 
-        it("should initialise with buttons", function() {
-            const form = document.createElement("form");
-            let button;
-            for (let i = 0; i < options.length; i++) {
-                button = document.createElement("button");
-                button.innerHTML = options[i].title;
-                form.appendChild(button);
-            }
-            expect(testGenDom.init().childNodes.length).not.toBeLessThan(0);
-        });
+    it('should update preview text on left', function () {
+      testGenDom.generator.addElement(testElement, true)
+      testGenDom.alignLeft(true)
+      testGenDom.updatePreview()
 
-        it("should update output", function() {
-            testGenDom.generator.addElement(testElement, true);
-            testGenDom.updateOutput();
+      expect(leftPreview.innerHTML).toEqual('[+]')
+    })
 
-            expect(output.value).toEqual("set laststatus=2set statusline+=%m");
-        });
+    it('should update preview text on right', function () {
+      testGenDom.generator.addElement(testElement, false)
+      testGenDom.alignLeft(false)
+      testGenDom.updatePreview()
 
-        it("should update preview text on left", function() {
-            testGenDom.generator.addElement(testElement, true);
-            testGenDom.alignLeft(true);
-            testGenDom.updatePreview();
+      expect(rightPreview.innerHTML).toEqual('[+]')
+    })
 
-            expect(leftPreview.innerHTML).toEqual("[+]");
-        });
+    it('should clear output and preview', function () {
+      output.value = 'fo'
+      leftPreview.value = 'ob'
+      rightPreview.value = 'ar'
+      testGenDom.clear()
 
-        it("should update preview text on right", function() {
-            testGenDom.generator.addElement(testElement, false);
-            testGenDom.alignLeft(false);
-            testGenDom.updatePreview();
-
-            expect(rightPreview.innerHTML).toEqual("[+]");
-        });
-
-        it("should clear output and preview", function() {
-            output.value = "fo";
-            leftPreview.value = "ob";
-            rightPreview.value = "ar";
-            testGenDom.clear();
-
-            expect(output.value).toEqual("");
-            expect(leftPreview.innerHTML).toEqual("");
-            expect(rightPreview.innerHTML).toEqual("");
-        });
-    });
-});
+      expect(output.value).toEqual('')
+      expect(leftPreview.innerHTML).toEqual('')
+      expect(rightPreview.innerHTML).toEqual('')
+    })
+  })
+})
